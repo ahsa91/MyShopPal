281d5f0a8d575846b5fd4e5559ccd2a72ab12c23
ahsa91 <ahsa91@gmail.com>
2022-03-14 01:30:12 +0000

    added splash screen

---
+++ app/src/main/java/ie/wit/myshoppal/SplashActivity.kt
@@ -0,0 +1,13 @@
+package ie.wit.myshoppal
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(applicationContext, MainActivity::class.java))//render activity after splash screen
+        finish()
+    }
+}
