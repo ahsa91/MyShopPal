99d01793083f895303de944a7567aa9e60b2d06e
ahsa91 <ahsa91@gmail.com>
2022-03-15 00:35:56 +0000

    different splash screen way

--- app/src/main/java/ie/wit/myshoppal/MainActivity.kt
+++ app/src/main/java/ie/wit/myshoppal/MainActivity.kt
@@ -1,7 +1,10 @@
 package ie.wit.myshoppal
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,3 +12,4 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
--- app/src/main/java/ie/wit/myshoppal/SplashActivity.kt
+++ app/src/main/java/ie/wit/myshoppal/SplashActivity.kt
@@ -1,14 +1,40 @@
 package ie.wit.myshoppal
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//I used https://www.youtube.com/watch?v=GtE8NwOQoos
-//for the splashscreen
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
+
+@Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(applicationContext, MainActivity::class.java))//render activity after splash screen
-        finish()
+        setContentView(R.layout.activity_splash)
+
+
+        //deprecated code to make splash screen as full screen activity
+        //source https://stackoverflow.com/questions/62835053/how-to-set-fullscreen-in-android-r
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        Handler().postDelayed(
+            {
+                // Launch the Main Activity
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))//render mainactivity
+                finish() // finish activity action
+            },
+            1000
+        )
+
     }
 }
