9229a147119d5ba89028c29cdf14a8e3ba2aeb98
ahsa91 <ahsa91@gmail.com>
2022-03-15 19:19:19 +0000

    prepare button and login activity

---
+++ app/src/main/java/ie/wit/myshoppal/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package ie.wit.myshoppal.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ie.wit.myshoppal.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/ie/wit/myshoppal/utils/MSPEditText.kt
@@ -0,0 +1,21 @@
+package ie.wit.myshoppal.utils
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class MSPEditText (context: Context, attrs: AttributeSet): AppCompatTextView(context, attrs) {
+
+    init {
+        applyFont()
+    }
+
+    private fun applyFont() {
+
+        // This is used to get the file from the assets folder and set it to the title textView.
+        val typeface: Typeface =
+            Typeface.createFromAsset(context.assets, "Montserrat-Bold.ttf")
+        setTypeface(typeface)
+    }
+}
